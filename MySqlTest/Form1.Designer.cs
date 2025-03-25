91f879c9a002b5079d932021a5b659d34c24c934
st50 <st50@WS50-15>
2025-03-25 13:15:35 +0300

    Добавьте файлы проекта.

---
+++ MySqlTest/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace MySqlTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mySqlCommand1
+            // 
+            this.mySqlCommand1.CacheAge = 0;
+            this.mySqlCommand1.Connection = null;
+            this.mySqlCommand1.EnableCaching = false;
+            this.mySqlCommand1.Transaction = null;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ MySqlTest/MySQLUsersReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlTest
+{
+    public class MySQLStudentsReader
+    {
+        public List<User> ReadUsers()
+        {
+            List<User> result = new List<User>();
+
+            string myConnectionString = "server=127.0.0.1;port=3306;uid=root;pwd=vertrigo;database=myvknetwork2;"; /// строка соединения с БД
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(myConnectionString))
+                {
+                    conn.Open();
+
+                    const string query = "SELECT id AS ИД, name, surname FROM students;";
+                    MySqlCommand command = new MySqlCommand(query, conn); /// объект команды
+                    using (MySqlDataReader reader = command.ExecuteReader()) /// запуск исполнения команды на сервере
+                    {
+                        while (reader.Read()) /// пока  есть данные в результате
+                        {
+                            int id = reader.GetInt32("ИД");
+
+                            User st = new User(id);
+                            st.Name = reader.GetString("name");
+                            st.Surname = reader.GetString("surname");
+                            result.Add(st);
+                        }
+                    }
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return result;
+            }
+
+            return result;
+        }
+    }
+}
